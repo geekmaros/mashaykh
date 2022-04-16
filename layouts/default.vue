2cdf09f6cf1f46f7318dfeedef6fd3fe0dda4340
amustapha <amustapha@linkfire.com>
2022-04-16 17:56:32 +0100

    feature/init files

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="h-screen w-full bg-amber-400">
+ <slot/>
+</div>
+</template>
+
+<script>
+export default {
+  name: "default"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container bg-red-200">
+    Welcome to the Vue.js app!
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index.vue",
+}
+</script>
+
+<style scoped>
+
+</style>
